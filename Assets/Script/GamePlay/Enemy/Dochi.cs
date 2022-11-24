f136a885375aa66a6ef0fb8ee49aa6bf4313d50a
SEOKYI <104981970+SeokYi16@users.noreply.github.com>
2022-11-24 16:06:16 +0900

    1124
    1124

--- Assets/Script/GamePlay/Enemy/Dochi.cs
+++ Assets/Script/GamePlay/Enemy/Dochi.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Dochi : EnemyInfo
 {
+    int enemyhp = 16;
+    int enemystr = 5;
+    int enemyspd = 4;
+    int enemydef = 3;
+
     public override void MonsterAtk(int monsterstr, int playerdef, int playerhp)
     {
         base.MonsterAtk(monsterstr, playerdef, playerhp);
