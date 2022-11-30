5d6ff6be0f4afb0aae936ce5038fde7a1a5f3b1f
SEOKYI <104981970+SeokYi16@users.noreply.github.com>
2022-11-30 16:39:52 +0900

    1130
    1130

--- Assets/Script/FightMananger.cs
+++ Assets/Script/FightMananger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FightMananger : MonoBehaviour
 {
@@ -11,8 +12,17 @@ public class FightMananger : MonoBehaviour
         enemySpawn = FindObjectOfType<EnemySpawn>();
     }
 
+    private void Update()
+    {
+        
+    }
+
     public void FirstTurn(int playerspd, int sub1, int sub2)//적 수에 따라 어떻게 추가해야하나
     {
-
+        int [] enemyspd = new int[enemySpawn.enemy_count.Count]; //적 스피드를 담기위해 적 수만큼 배열을 생성
+        for(int i = 0; i < enemySpawn.enemy_count.Count; i++)
+        {
+            enemyspd[i] = enemySpawn.enemy_count[i].GetComponent<EnemyInfo>().enemyspd; //적 스피드를 담음
+        }
     }
 }
--- Assets/Script/GamePlay/Enemy/EnemyInfo.cs
+++ Assets/Script/GamePlay/Enemy/EnemyInfo.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class EnemyInfo : MonoBehaviour
 {
@@ -10,6 +12,34 @@ public class EnemyInfo : MonoBehaviour
     int[] enemyspd = { 7, 4, 5, 11, 10, 13 };
     int[] enemydef = { 5, 3, 4, 6, 7, 9 };
     */
+
+    //각 적 정보를 입력
+    public int enemyhp;
+    public int enemystr;
+    public int enemyspd;
+    public int enemydef;
+    public int enemymaxhp;
+    public bool isAtk;
+
+    public Slider enemy_slider;
+    public TextMeshProUGUI enemy_hp_text;
+
+    private void Start()
+    {   //적 hp = 최대 hp 와 슬라이더 max값을 적 maxhp로 바꿈
+        enemyhp = enemymaxhp;
+        enemy_slider.maxValue = enemymaxhp;
+    }
+
+    private void Update()
+    {   //실시간 적 체력 표기
+        enemy_slider.value = enemyhp;
+        enemy_hp_text.text = enemyhp + " / " + enemymaxhp;
+        //체력이 0이면 오브젝트 파괴
+        if (enemyhp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     public virtual void MonsterAtk(int monsterstr, int playerdef, int playerhp)
     {
