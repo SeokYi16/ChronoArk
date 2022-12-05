b7c260eccd30088901492e37467af6b75a32effd
SEOKYI <104981970+SeokYi16@users.noreply.github.com>
2022-12-05 16:35:10 +0900

    1205
    1205

--- Assets/Script/FightMananger.cs
+++ Assets/Script/FightMananger.cs
@@ -7,6 +7,16 @@ public class FightMananger : MonoBehaviour
 {
     EnemySpawn enemySpawn;
 
+    public Slider player_TurnSlider;
+    public Slider azar_TurnSlider;
+    public Slider joey_TurnSlider;
+
+    public PlayerStat playerStat;
+    public AzarStat azarStat;
+    public JoeyStat joeyStat;
+
+    public bool turn_start = false;
+
     private void Awake()
     {
         enemySpawn = FindObjectOfType<EnemySpawn>();
@@ -14,7 +24,7 @@ public class FightMananger : MonoBehaviour
 
     private void Update()
     {
-        
+        SliderCount();
     }
 
     public void FirstTurn(int playerspd, int sub1, int sub2)//적 수에 따라 어떻게 추가해야하나
@@ -24,5 +34,29 @@ public class FightMananger : MonoBehaviour
         {
             enemyspd[i] = enemySpawn.enemy_count[i].GetComponent<EnemyInfo>().enemyspd; //적 스피드를 담음
         }
+    }
+
+    void SliderCount()
+    {
+        if (!turn_start)
+        {
+            player_TurnSlider.value -= Time.deltaTime * 10 * playerStat.speed;
+            azar_TurnSlider.value -= Time.deltaTime * 10 * azarStat.speed;
+            joey_TurnSlider.value -= Time.deltaTime * 10 * joeyStat.speed;
+        }
+        else
+        {
+
+        }
+
+        if(player_TurnSlider.value <= 0 || azar_TurnSlider.value <= 0 || joey_TurnSlider.value <=0)
+        {
+            turn_start = true;
+        }
+        else
+        {
+            turn_start = false;
+        }
+
     }
 }
